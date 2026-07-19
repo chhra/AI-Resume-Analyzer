0b0fea4bacd965188654d993cd0c2527636ce70d
Chahrazed Bousmaha <bousmahachahrazed2906@gmail.com>
2026-07-19 22:09:49 +0100

    Authentification

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,4 +1,7 @@
 import type { Route } from "./+types/home";
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
+import { usePuterStore } from "~/lib/puter";
 import Navbar from "~/components/Navbar";
 import { resumes } from "../../constants";
 import ResumeCard from "~/components/ResumeCard";
@@ -11,9 +14,18 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+  const { auth } = usePuterStore();
+  const navigate = useNavigate();
+  //user tries to access homepage, but he is not authenticated, we bring him back to auth
+  useEffect(() => {
+    if (!auth.isAuthenticated) {
+      navigate("/auth?next=");
+    }
+  }, [auth.isAuthenticated]);
   return (
     <main className="bg-[url('/images/bg-main.svg')] bg-cover">
       <Navbar />
+
       <section className="main-section">
         <div className="page-heading py-16">
           <h1>Track Your Applications & Resume Ratings</h1>
